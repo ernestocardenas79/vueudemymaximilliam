5528bc5c19b7626df76058dbfb1af4c87f749100
ernestocardenas79 <rnst79@DESKTOP-D2H79NF.localdomain>
2019-07-12 20:13:10 -0600

    =terminado el ejecicio 4

--- section2/Exercise 4/src/app.js
+++ section2/Exercise 4/src/app.js
@@ -1,14 +1,35 @@
 new Vue({
   el: '#exercise',
   data: {
-
-  },
-  computed:{
-    
+    effect:true,
+    effectClasses: {
+      highlight:true,
+      shrink:false
+    },
+    likethat:'',
+    apply:false,
+    other:'',
+    estilacho:'',
+    progress:{'width':'0px', 'height': '20px', 'background-color': 'blue'}
   },
   methods: {
     startEffect: function() {
-    
+      var ex = this;
+      setInterval(function(){
+        ex.effectClasses = {
+          highlight:!ex.effect,
+          shrink:ex.effect
+        };
+        ex.effect= !ex.effect
+      },1000)
+    },
+    starProgress: function(){
+      var ex = this;
+      let width=0;
+      setInterval(function(){
+        ex.progress.width = width+'px';
+        width=width+10;
+      },500)
     }
   }
 });
