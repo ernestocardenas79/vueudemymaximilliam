8df16ba04e15b4d0d309d30a9f8b50f861451a08
Ernesto Cardenas Llanes <ernesto.llanes@neoris.cxnetworks.net>
2019-07-24 16:00:06 -0600

    Ejercicio de componentes dinamic y slots terminado

--- section9/Start/src/App.vue
+++ section9/Start/src/App.vue
@@ -1,32 +1,50 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <br>
-                <button class="btn btn-primary">Load Blue Template</button>
-                <button class="btn btn-success">Load Green Template</button>
-                <button class="btn btn-danger">Load Red Template</button>
-                <hr>
-                <app-blue></app-blue>
-                <app-green></app-green>
-                <app-red></app-red>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <br />
+        <button class="btn btn-primary" @click="selectedComponent='app-blue'">Load Blue Template</button>
+        <button class="btn btn-success" @click="selectedComponent='app-green'">Load Green Template</button>
+        <button class="btn btn-danger" @click="selectedComponent='app-red'">Load Red Template</button>
+        <hr />
+        <keep-alive>
+          <component :is="selectedComponent">
+            <h1 v-if="selectedComponent == 'app-blue'">Muajaaaaaa</h1>
+            <h1 v-if="selectedComponent == 'app-green'">History</h1>
+            <h1 v-if="selectedComponent == 'app-red'">Madness</h1>
+          </component>
+        </keep-alive>
+        <!-- <app-blue>
+          <h1>Muajaaaaaa</h1>
+        </app-blue>
+        <app-green>
+          <h1>History</h1>
+        </app-green>
+        <app-red>
+          <h1>Madness</h1>
+        </app-red>-->
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import Blue from './components/Blue.vue';
-    import Green from './components/Green.vue';
-    import Red from './components/Red.vue';
+import Blue from "./components/Blue.vue";
+import Green from "./components/Green.vue";
+import Red from "./components/Red.vue";
 
-    export default {
-        components: {
-            appBlue: Blue,
-            appGreen: Green,
-            appRed: Red
-        }
-    }
+export default {
+  data: function() {
+    return {
+      selectedComponent: "app-blue"
+    };
+  },
+  components: {
+    appBlue: Blue,
+    appGreen: Green,
+    appRed: Red
+  }
+};
 </script>
 
 <style>
