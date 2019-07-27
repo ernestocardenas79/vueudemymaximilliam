2700049272328586ef1a3610d1f73327605f9a74
ernestocardenas79 <rnst79@DESKTOP-D2H79NF.localdomain>
2019-07-27 13:25:25 -0600

    ejerciocio de directivas customizadas

---
+++ section12/Start/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1 v-my-on:click="clicked">Directives Exercise</h1>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+                <div style="width:100px; height:300px;background-color:aqua"
+                    v-my-on:mouseenter="onMousEenter"
+                    v-my-on:mouseleave="onMousLeave"
+                >
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods:{
+            clicked(){console.log('clicked')},
+            onMousEenter(){console.log('onMousEenter')},
+            onMousLeave(){console.log('onMousLeave')},
+        },
+        directives:{
+            'my-on':{
+                bind(el, binding,vnode){
+                    let type = binding.arg;
+                    let fn = binding.value;
+                    el.addEventListener(type, fn);
+                }
+            }
+        }
+    }
+
+</script>
+
+<style>
+</style>
